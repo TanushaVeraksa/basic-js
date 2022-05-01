640ec4c85cf695d74f4fc52ebd434f87dbdc6856
tanushaveraksa <veraksa161@gmail.com>
2022-05-01 05:54:25 +0300

    featL create extended-repeat

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let separator = options.separator;
+  let additionSeparator = options.additionSeparator;
+  let strTmp = String(str);
+  let addition = options.addition;
+   if(options.separator === undefined) {
+     separator = '+';
+   }
+  if(options.additionSeparator === undefined) {
+    additionSeparator = '|';
+  }
+
+  if(options.separator === undefined) {
+    separator = '+';
+  }
+  if(options.repeatTimes === undefined) {
+    strTmp = strTmp + separator;
+    return strTmp;
+  }
+  if(options.repeatTimes && options.additionRepeatTimes === undefined) {
+    let tmp = strTmp + separator;
+    for(let i = 0; i<options.repeatTimes - 1; i++) {
+      tmp += strTmp + separator;
+    }
+    return tmp.split(separator).filter((elem) => elem != '').join(separator)
+  }
+  if(options.repeatTimes && options.additionRepeatTimes) {
+    let resTmp = str + addition + additionSeparator;
+    let tmp = ''
+    for(let i = 0; i<options.additionRepeatTimes; i++) {
+      tmp += resTmp;
+    }
+    let tmp1 = tmp.split(additionSeparator).filter((elem) => elem != '').join(additionSeparator)
+    let tmp2 = '';
+    for(let i = 0; i<options.repeatTimes; i++) {
+      tmp2 += tmp1 + separator;
+    }
+    return tmp2.split(separator).filter((elem) => elem != '').join(separator);
+  }
 }
 
 module.exports = {
