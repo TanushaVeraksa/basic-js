b12023824178c83c7609a549189733d85bc2e677
tanushaveraksa <veraksa161@gmail.com>
2022-10-17 21:01:57 +0300

    fix: change extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,38 +15,69 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
- function repeater(str, options) {
-  let newStr = String(str);
-  let result = '';
-  if(options.separator === undefined) {
-    options.separator = '+';
-  }
-  if(options.additionSeparator === undefined) {
-    options.additionSeparator = '|';
-  }
-  if(options.repeatTimes === undefined &&  options.additionRepeatTimes === undefined) {
-    result = newStr + options.addition;
-  }
-  if(options.repeatTimes && options.additionRepeatTimes === undefined) {
-    for(let i = 0; i<options.repeatTimes; i++) {
-        result += newStr + options.separator;
-    }
-    result = result.split(options.separator).slice(0, options.repeatTimes).join(options.separator);
-  }
-  if(options.repeatTimes && options.additionRepeatTimes) {
-    let res1 = '';
-    for(let i = 0; i<options.additionRepeatTimes; i++) {
-        res1 += newStr + options.addition + options.additionSeparator;
-    }
+//  function repeater(str, options) {
+//   let newStr = String(str);
+//   let result = '';
+//   if(options.separator === undefined) {
+//     options.separator = '+';
+//   }
+//   if(options.additionSeparator === undefined) {
+//     options.additionSeparator = '|';
+//   }
+//   if(options.repeatTimes === undefined &&  options.additionRepeatTimes === undefined) {
+//     result = newStr + options.addition;
+//   }
+//   if(options.repeatTimes && options.additionRepeatTimes === undefined) {
+//     for(let i = 0; i<options.repeatTimes; i++) {
+//         result += newStr + options.separator;
+//     }
+//     result = result.split(options.separator).slice(0, options.repeatTimes).join(options.separator);
+//   }
+//   if(options.repeatTimes && options.additionRepeatTimes) {
+//     let res1 = '';
+//     for(let i = 0; i<options.additionRepeatTimes; i++) {
+//         res1 += newStr + options.addition + options.additionSeparator;
+//     }
    
-    res1 =  res1.split(options.additionSeparator).slice(0, options.additionRepeatTimes).join(options.additionSeparator);
-    let res2 = '';
-    for(let i = 0; i<options.repeatTimes; i++) {
-        res2 += res1 + options.separator;
+//     res1 =  res1.split(options.additionSeparator).slice(0, options.additionRepeatTimes).join(options.additionSeparator);
+//     let res2 = '';
+//     for(let i = 0; i<options.repeatTimes; i++) {
+//         res2 += res1 + options.separator;
+//     }
+//     result = res2.split(options.separator).slice(0, options.repeatTimes).join(options.separator);
+//   }
+//   return result;
+// }
+
+function repeater(str, options) {
+  function h(add, separator, length=1) {
+    let s = separator === 'undefined' ? '|' : separator;
+    let res = '';
+    for(let i = 0; i<length; i++) {
+      if(add === 'undefined') {
+        res += s;
+      } else {
+        res += add + s;
+      }
     }
-    result = res2.split(options.separator).slice(0, options.repeatTimes).join(options.separator);
+    return res.split(s).filter((elem)=> elem != '' && elem != 'undefined').join(s);
   }
-  return result;
+  function h2(str, separator, curr, length) {
+    let s = separator === 'undefined' ? '+' : separator;
+    let tmp = curr.split(s);
+    let res = '';
+    for(let i = 0; i<length; i++) {
+      if(tmp[i] === 'undefined') {
+        res +=  str + s;
+      } else {
+        res += str + tmp[i] + s;
+      }
+    }
+    return res.split(s).filter((elem)=> elem != '' && elem != 'undefined').join(s);
+  }
+  let current = h(String(options.addition), String(options.additionSeparator), options.additionRepeatTimes);
+  let sec = h(current, String(options.separator), options.repeatTimes);
+  return h2(String(str), String(options.separator), sec, options.repeatTimes);
 }
 
 module.exports = {
